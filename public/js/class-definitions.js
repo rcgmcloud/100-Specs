ea090ae2931011b56fb3c9e90d41d7224ebe4bd7
Raymond Mariano <rcm@hawaii.edu>
2015-05-18 22:09:59 -1000

    first commit: progress step 35

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -3,7 +3,7 @@
  * Declare a variable named "unicorn"
  *
  */
-
+var unicorn = null;
 
 /* Step 2
  *
@@ -11,7 +11,7 @@
  * then assign it to be the value of "Atari"
  *
  */
-
+var videogame_system = "Atari";
 
 /* Step 3
  *
@@ -19,7 +19,7 @@
  * and assign the value of sahara_animal to "The Addax"
  *
  */
-
+var sahara_animal = "The Addax";
 
 /* Step 4
  *
@@ -29,7 +29,7 @@
  * Capitalize the names.
  */
 
-
+var planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
 /* Step 5
  *
  * Declare a variable named "galilean_moons"
@@ -37,7 +37,7 @@
  * each galilean moon of Jupiter
  *
  */
-
+var galilean_moons = [ 'Io', 'Europa', 'Ganymede', 'Callisto'];
 
 /* Step 6 φ
  *
@@ -45,7 +45,7 @@
  * and assign the value of golden_ratio to 1.61803398874
  *
  */
-
+var golden_ratio = 1.61803398874;
 
 /* Step 7
  *
@@ -53,7 +53,7 @@
  * and assign the value of earths_water_composition to 0.71
  *
  */
-
+var earths_water_composition = 0.71;
 
 /* Step 8
  *
@@ -61,7 +61,7 @@
  * and assign the value of club_name to "Fight Club"
  *
  */
-
+var club_name = "Fight Club";
 
 /* Step 9
  *
@@ -72,7 +72,11 @@
  *   male    => "male"
  *   unknown => undefined
  */
-
+var gender = {
+  female: 'female',
+  male: 'male',
+  unknown: undefined
+};
 
 /* Step 10
  *
@@ -86,6 +90,12 @@
  *   gender  => gender.female
  *
  */
+var princess_leia = {
+  name: 'Leia Organa',
+  money: 890,
+  age: 20,
+  gender: gender.female
+};
 
 
 /* Step 11
@@ -99,6 +109,12 @@
  *   jsonformatter.curiousconcept.com => "104.28.5.70"
  *
  */
+var domains = {
+  "ycombinator.com": "198.41.190.47",
+  "laughingsquid.com": "162.159.247.97",
+  "slumlordhosting.com" :"198.61.179.126",
+  "jsonformatter.curiousconcept.com": "104.28.5.70"
+};
 
 
 /* Step 12
@@ -114,7 +130,14 @@
  *   Konqueror  => "konqueror.org"
  *
  */
-
+var browsers = {
+  Chromium: "google.com",
+  Safari: "apple.com",
+  Opera: "opera.com",
+  Firefox: "mozilla.org",
+  Sleipnir: "fenrir-inc.com",
+  Konqueror: "konqueror.org"
+}
 
 /* Step 13
  *
@@ -132,13 +155,25 @@
  *  isDouble  => true
  *
  */
+var rainbow  = {
+  colors: {
+    red : "#F0280A",
+    orange: "#FF8800",
+    yellow: "#FFDD00",
+    green: "#51AB0C",
+    blue: "#1593ED",
+    indigo: "#5215ED",
+    violet: "#A915ED",
+  },
+  isDouble: true
 
+}
 
 /* Step 14
  *
  * Declare an array named "linuxFlavors"
  * with the following values:
- * 
+ *
  * "Gentoo"
  * "Fedora"
  * "Debian"
@@ -147,13 +182,13 @@
  * "Bieber Linux"
  *
  */
-
+var linuxFlavors = ["Gentoo", "Fedora", "Debian", "Slackware", "Red Hat", "Bieber Linux"];
 
 /* Step 15
  *
  * Declare an array named "DNA" with the
  * following values:
- * 
+ *
  * "nucleatides"
  * "guanine"
  * "adenine"
@@ -161,26 +196,31 @@
  * "cytosine"
  *
  */
-
+var DNA = ["nucleatides", "guanine", "adenine", "thymine", "cytosine"];
 
 /* Step 16
  *
  * Define a literal object named "laptopCosts"
  * with properties and values set to:
- * 
+ *
  * MacBook      => 1500
  * Alienware    => 2500
  * HP           => 499
  * Surface      => 320
  *
  */
-
+var laptopCosts = {
+  MacBook: 1500,
+  Alienware: 2500,
+  HP: 499,
+  Surface: 320
+};
 
 /* Step 17
  *
  * Declare an array named "livingOrganismClassification"
  * with the following values:
- * 
+ *
  * "Kingdom"
  * "Phylum"
  * "Class"
@@ -190,13 +230,21 @@
  * "Species"
  *
  */
-
+var livingOrganismClassification = [
+  "Kingdom",
+  "Phylum",
+  "Class",
+  "Order",
+  "Family",
+  "Genus",
+  "Species"
+];
 
 /* Step 18
  *
  * Declare an array named "scmList" with
  * the following values:
- * 
+ *
  * "git"
  * "svn"
  * "mercurial"
@@ -204,13 +252,19 @@
  * "cvs"
  *
  */
-
+var scmList = [
+"git",
+"svn",
+"mercurial",
+"bazaar",
+"cvs"
+];
 
 /* Step 19
  *
  * Define a literal object named "beers" with
  * properties and values set to:
- * 
+ *
  * IPA          => "Ale"
  * Lager        => "Strong"
  * Heffeweisen  => "German"
@@ -223,7 +277,14 @@
  *                  "Golden"
  *
  */
-
+var beers = {
+  IPA: "Ale",
+  Lager: "Strong",
+  Heffeweisen: "German",
+  Stout: ["Thick", "Dark"],
+  Porter: "Bitter",
+  Ale: ["Light", "Golden"]
+};
 
 /* Step 20
  *
@@ -233,19 +294,23 @@
  * @return {String}
  *
  */
-
+function sahara_river(){
+  return 'Nile River';
+}
 
 /* Step 21
  *
  * Define a function named "addNumbers"
  * that returns the sum of two numbers
- * 
+ *
  * @param {Number}
  * @param {Number}
  * @return {Number}
  *
  */
-
+function addNumbers(x, y){
+  return x + y;
+}
 
 /* Step 22
  *
@@ -253,12 +318,17 @@
  * that takes a type of Linux as a parameter,
  * checks if it exists in "linuxFlavors" and
  * returns success or failure
- * 
+ *
  * @param {String}
  * @return {Bool}
  *
  */
-
+function installLinux(type){
+  if(linuxFlavors.indexOf(type) !== -1){
+    return true;
+  };
+  return false;
+}
 
 /* Step 23
  *
@@ -266,18 +336,29 @@
  * a type of beer as a paramater. If it is not
  * a valid type of beer as defined in "beers",
  * return false. If it is valid, return the following phrases:
- * 
+ *
  * "This {beerName} is {beerDescription}."
- * 
+ *
  * Or, if there are multiple descriptions:
- * 
+ *
  * "This {beerName is {beerDescription1} and {beerDescription2} and..."
- * 
+ *
  * @param {String}
  * @return {Bool when False, String when True}
  *
  */
-
+function drink(type){
+  if(beers[type] === undefined){
+    return false;
+  }
+  if (typeof beers[type] === "string"){
+    return "This " + type + " is " + beers[type] + ".";
+  }
+   if (typeof beers[type] === "object"){
+    return "This " + type + " is " + beers[type].join(" and ") + ".";
+  }
+  return true;
+}
 
 /* Step 24
  *
@@ -285,23 +366,30 @@
  * a browser and validates it, as defined by "browsers"
  * If the browser doesn't exist, return false.
  * If the browser exists, return the value which is a partial URL.
- * 
+ *
  * @param {String}
  * @return {String if true else return false}
  *
  */
-
+function browseURL(x){
+  if(browsers[x] === undefined){
+    return false;
+  }
+  return browsers[x];
+}
 
 /* Step 25
  *
  * Define a function named "listLivingOrgClass" that
  * returns an HTML-formatted unordered list (<ul>) of all
  * living organism classifications as an list element(<li>)
- * 
+ *
  * @return {String}
  *
  */
-
+function listLivingOrgClass(){
+  return '<ul><li>' + livingOrganismClassification.join('</li><li>') + '</li></ul>';
+}
 
 /* Step 26
  *
@@ -309,20 +397,25 @@
  * takes in the person's current planet and validates
  * that the planet exists. If it doesn't, return the
  * following message:
- * 
+ *
  * "{currentPlanet} is not a planet!"
- * 
+ *
  * If the planet does exist, select a different, random
  * planet from the "planets" variable. Once selected,
  * return the following message:
- * 
+ *
  * "I'm from {currentPlanet}, but I wish I could go to {randomPlanet}."
- * 
+ *
  * @param {String}
  * @return {String}
  *
  */
-
+function favoritePlanet(x){
+  if(planets.indexOf(x) === -1){
+    return x + ' is not a planet!';
+  }
+  return "I'm from " + x + ", but I wish I could go to " + planets[Math.floor(Math.random()*planets.length)];
+}
 
 /* Step 27
  *
@@ -332,7 +425,7 @@
  * named "spendMoney" and "earnMoney" that each take an
  * amount of money and add or subtract them to this
  * person's money.
- * 
+ *
  * class
  *   Person
  * properties
@@ -340,12 +433,30 @@
  *   @param {String} money
  *   @param {String} age
  *   @param {String} gender
- * 
+ *
  * methods
  *   spendMoney
  *   earnMoney
  *
  */
+function Person(name, money, age, gender){
+  this.name = name;
+  this.money = money;
+  this.age = age;
+  this.gender = gender;
+  // this.spendMoney = function(x){
+  //   this.money = this.money - x;
+  // };
+  // this.earnMoney = function(x){
+  //   this.money = this.money + x;
+  // };
+}
+Person.prototype.spendMoney = function(x) {
+  this.money -= x;
+};
+Person.prototype.earnMoney = function(x) {
+  this.money += x;
+};
 
 
 /* Step 28
@@ -354,12 +465,17 @@
  * a laptop as a parameter. If the laptop is valid as
  * defined in "laptopCosts" then return the cost as a string. Otherwise,
  * return -1
- * 
+ *
  * @param {String}
  * @return {String}
  *
  */
-
+function purchaseLaptop(laptop){
+  if(laptopCosts[laptop] === undefined){
+    return -1;
+  }
+  return laptopCosts[laptop].toString();
+}
 
 /* Step 29
  *
@@ -367,62 +483,87 @@
  * a club as a parameter. Return true if you can
  * talk about the club and false if the club passed
  * in has the same value as "club_name"
- * 
+ *
  * @param {String}
  * @return {Bool}
  *
  */
-
+function canTalkAbout(club){
+  if(club === club_name){
+    return false;
+  }
+  return true;
+}
 
 /* Step 30
  *
  * Define a class named "Pen" with a property for
  * color and a class method named "write" that takes
  * a message string as a parameter and returns the
- * string back with the pen's color added to the 
+ * string back with the pen's color added to the
  * beginning of the string
  *
  * example: "{color}: {Message passed in}"
- * 
+ *
  * class
  *   Pen
- *   
+ *
  * properties
  *   @param {String} color
- *   
+ *
  * methods
  *   write
  *
  */
+function Pen(color){
+  this.color = color;
+}
 
+Pen.prototype.write = function(message) {
+  return this.color + ": " + message;
+};
 
 /* Step 31
  *
  * Define a class named "Garden" with a property for
  * the number of plants and whether it was recently
- * watered. 
- * 
+ * watered.
+ *
  * Define a class method named "water" which
  * sets the recently watered property to true
- * 
- * Defined a class method named "grow" which adds a plant to 
+ *
+ * Defined a class method named "grow" which adds a plant to
  * the garden if it was recently watered and exhausts the water.
- * If this method is called and the 'isWatered' property is 
+ * If this method is called and the 'isWatered' property is
  * false, it should return false;
- * 
+ *
  * class
  *   Garden
- *   
+ *
  * properties
- *   @param {Number} plantsTotal   
- *   @property {Bool:Not Passed as Argument - Default Value FALSE} isWatered 
- *   
+ *   @param {Number} plantsTotal
+ *   @property {Bool:Not Passed as Argument - Default Value FALSE} isWatered
+ *
  * methods
  *   water
  *   grow
- * 
+ *
  */
+function Garden(plantsTotal){
+  this.plantsTotal = plantsTotal;
+  this.isWatered = false;
+}
 
+Garden.prototype.water = function() {
+  this.isWatered = true;
+};
+Garden.prototype.grow = function() {
+  if(this.isWatered === true){
+      this.plantsTotal++;
+      this.isWatered = false;
+    }
+    return false;
+};
 
 /* Step 32
  *
@@ -430,7 +571,7 @@
  * "planets" that is initialized as an empty array. Add
  * a method "addPlanet" that adds a planet to the list
  * and a method "removePlanet" that removes a planet.
- * 
+ *
  * class
  *   SolarSystem
  * properties
@@ -440,41 +581,73 @@
  *   removePlanet
  *
  */
-
+function SolarSystem(){
+  this.planets = [];
+}
+SolarSystem.prototype.addPlanet = function(x) {
+  this.planets.push(x);
+};
+SolarSystem.prototype.removePlanet = function(x) {
+  this.planets.splice(this.planets.indexOf(x), 1);
+};
 
 /* Step 33
  *
  * Define a class named "PrincessLeia" that is a subclass of "Person"
  * with properties "name," "money," "age," "gender," and
  * "isInTrouble"(initialized as null)
- * 
+ *
  * Add a method "shootsGun" that returns a message
  * "Leia shoots her gun wildly"
  * and sets the isInTrouble property to FALSE
- * 
+ *
  * Add a method "getsInTrouble" that returns a message
  * "Help me Obi-wan Kenobi, you're my only hope"
  * and sets the property 'isInTroube' to TRUE
- * 
+ *
  * Add a method "marries" that takes a prospective
  * love interest and returns true for "Han Solo" or false for anyone
  * else.
- * 
+ *
  * In the 'marries' method - Add a message of disapproval for "Luke Skywalker"
  * "Gross!"
- * 
- * 
+ *
+ *
  * class
  *   PrincessLeia
  * properties
  *   isInTrouble
  * methods
- *   shootsGun 
+ *   shootsGun
  *   getsInTrouble
  *   marries
  *
  */
+function PrincessLeia(name, money, age, gender){
+  Person.call(this, name, money, age, gender);
+  this.isInTrouble = null;
+};
 
+PrincessLeia.prototype = Object.create(Person.prototype);
+
+PrincessLeia.prototype.shootsGun = function() {
+  this.isInTrouble = false;
+  return "Leia shoots her gun wildly";
+};
+
+PrincessLeia.prototype.getsInTrouble = function() {
+  this.isInTrouble = true;
+    return "Help me Obi-wan Kenobi, you're my only hope";
+};
+PrincessLeia.prototype.marries = function(x) {
+  if(x === 'Han Solo'){
+      return true;
+    }
+    if(x === 'Luke Skywalker'){
+      return 'Gross!';
+    }
+    return false;
+};
 
 /* Step 34
  *
@@ -483,7 +656,7 @@
  * number of papers as a parameter and returns true if the
  * papers are less than or equal to the papers and false
  * if there are too many papers to staple.
- * 
+ *
  * class
  *   Stapler
  * properties
@@ -493,7 +666,17 @@
  *   staplePapers
  *
  */
+function Stapler(color, maxPapers){
+  this.color = color;
+  this.maxPapers = maxPapers;
+}
 
+Stapler.prototype.staplePapers = function(numPapers) {
+   if(numPapers <= this.maxPapers){
+      return true;
+    }
+    return false;
+};
 
 /* Step 35
  *
@@ -505,11 +688,11 @@
  * adds it to the discipline property. Return the value of the discipline property
  *
  * Add a method named 'checkDisciple' that takes a string as an argument and
- * checks if the argument can be found in the discipline property. Return false 
+ * checks if the argument can be found in the discipline property. Return false
  * if it is not found otherwise return back true.
  *
  * Add a method named 'addDiscovery' that takes a string as an argument and
- * adds it to the discoveries property. Return back the contents of the discoveries 
+ * adds it to the discoveries property. Return back the contents of the discoveries
  * property as a String made up of all the elements found see below
  *
  * examples:
@@ -518,10 +701,10 @@
  *
  * ["Gravity", "Theory of Relativity"] will be returned as:
  * "I discovered Gravity and Theory of Relativity."
- * 
+ *
  * ["Gravity", "Theory of Relativity", "Jesus Christ"] will be returned as:
  *       "I discovered Gravity, Theory of Relativity, and Jesus Christ."
- * 
+ *
  * class
  *   Scientist
  * properties
@@ -531,10 +714,40 @@
  *   addDiscipline
  *   checkDiscipline
  *   addDiscovery
- *   
+ *
  */
+function Scientist(name, money, age, gender){
+  Person.call(this, name, money, age, gender);
+  this.disciplines = [];
+  this.discoveries = [];
+}
 
+Scientist.prototype = Object.create(Person.prototype);
 
+Scientist.prototype.addDiscipline = function(x) {
+  this.disciplines.push(x);
+    return this.displines;
+};
+
+Scientist.prototype.checkDiscipline = function(x) {
+  if(this.disciplines.indexOf(x) === -1){
+      return false;
+    }
+    return true;
+};
+
+// Scientist.prototype.addDiscovery = function(x) {
+//   this.discoveries.push(x);
+//   if(this.discoveries.length === 1){
+//     return "I discovered " + this.discoveries + ".";
+//   }
+//   if(this.discoveries.length === 2){
+//     return "I discovered " this.discoveries.join(" and ") + ".";
+//   }
+//   if(this.discoveries.length >= 3){
+//     return
+//   }
+// };
 /* Step 36
  *
  * Define a class named "BankAccount" that has properties
@@ -543,7 +756,7 @@
  * their Person object, "deposit" that lets a Person spend
  * money to increase their balance, and "rob" that allows
  * any Person object to take money from the bank account.
- * 
+ *
  * class
  *   BankAccount
  * properties
@@ -723,7 +936,7 @@
 
 /**
  * Step 50
- * 
+ *
  * Animal class
  * @param {string} species The animal species
  * @param {string} gender  male or female
@@ -732,7 +945,7 @@
 
 /**
  * Step 51
- * 
+ *
  * Vehicle class
  * @param {string} make The vehicle's make
  * @param {string} model The vehicle's model
@@ -741,18 +954,18 @@
 
 /**
  * Step 52
- * 
+ *
  * Shape class
- * @param {number} sides The number of sides, should be a number 
+ * @param {number} sides The number of sides, should be a number
  * greater or equal to 3, if the number is less than 3 then set the
  * value of the sides property to null.
- * 
+ *
  */
 
 
 /**
  * Step 53
- * 
+ *
  * Box class
  * @param {anything} contents The contents of the box
  * @param {boolean} isOpen     Whether the box is opened or closed
@@ -761,7 +974,7 @@
 
 /**
  * Step 54
- * 
+ *
  * Door class
  * @param {boolean} isOpen Whether the door is opened or closed
  */
@@ -769,7 +982,7 @@
 
 /**
  * Step 55
- * 
+ *
  * Shoe class
  * @param {number} size  The shoe size
  * @param {string} color The shoe color
@@ -778,7 +991,7 @@
 
 /**
  * Step 56
- * 
+ *
  * House class
  * @param {number} stories How many stories tall the house is
  */
@@ -786,7 +999,7 @@
 
 /**
  * Step 57
- * 
+ *
  * Lightbulb class
  * @param {boolean} isOn Whether the light is on or off
  */
@@ -794,7 +1007,7 @@
 
 /**
  * Step 58
- * 
+ *
  * Cookie class
  * @param {string} flavor The cookie's flavor
  */
@@ -802,7 +1015,7 @@
 
 /**
  * Step 59
- * 
+ *
  * Meal class
  * @param {Array} foods All the foods in the meal
  */
